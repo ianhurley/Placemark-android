238fe9c4173216c07e607cf6d74d63443bbfbe7b
Ianbitb <ian.hurley@gmail.com>
2023-09-23 17:48:28 +0100

    placemark-00

---
+++ app/src/main/java/ie/setu/placemark/PlacemarkActivity.kt
@@ -0,0 +1,16 @@
+package ie.setu.placemark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import timber.log.Timber
+import timber.log.Timber.Forest.i
+
+class PlacemarkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_placemark)
+
+        Timber.plant(Timber.DebugTree())
+        i("Placemark Activity started...")
+    }
+}
