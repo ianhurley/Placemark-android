c67020ce07f1e6ff6d3ca391bdf461aa1a346bf6
Ianbitb <ian.hurley@gmail.com>
2023-10-03 15:37:47 +0100

    placemark-02

--- app/src/main/java/ie/setu/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/ie/setu/placemark/activities/PlacemarkActivity.kt
@@ -2,39 +2,60 @@ package ie.setu.placemark.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
+import ie.setu.placemark.R
 import ie.setu.placemark.databinding.ActivityPlacemarkBinding
+import ie.setu.placemark.main.MainApp
 import ie.setu.placemark.models.PlacemarkModel
 import timber.log.Timber
 import timber.log.Timber.Forest.i
 
 class PlacemarkActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityPlacemarkBinding
     var placemark = PlacemarkModel()
-    val placemarks = ArrayList<PlacemarkModel>()
+    lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlacemarkBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Timber.plant(Timber.DebugTree())
-        i("Placemark Activity started...")
 
+        binding.toolbarAdd.title = title
+        setSupportActionBar(binding.toolbarAdd)
+
+        app = application as MainApp
+        i("Placemark Activity started...")
         binding.btnAdd.setOnClickListener() {
             placemark.title = binding.placemarkTitle.text.toString()
             placemark.description = binding.description.text.toString()
-
             if (placemark.title.isNotEmpty()) {
-                placemarks.add(placemark.copy())
+                app.placemarks.add(placemark.copy())
                 i("add Button Pressed: ${placemark}")
-                for (i in placemarks.indices)
-                { i("Placemark[$i]:${this.placemarks[i]}") }
+                for (i in app.placemarks.indices) {
+                    i("Placemark[$i]:${this.app.placemarks[i]}")
+                }
+                setResult(RESULT_OK)
+                finish()
             }
             else {
                 Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
                     .show()
             }
         }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_placemark, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_cancel -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
